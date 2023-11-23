e78c2c650c0d712d5192cf146c15847ce297e0b5
mingo99 <2839131656@qq.com>
2023-11-23 16:27:45 +0800

    write script to get actual ofm

--- scripts/python/compare.py
+++ scripts/python/compare.py
@@ -4,7 +4,7 @@ import re
 import numpy as np
 
 
-def read_ofm_line(file, ofm_size, cho, tile):
+def read_ofm_line(file, ofm_size, cho, tile_len):
     with open(file) as f:
         data = re.sub(r"\n", "", f.read())
         ofm = data.split(",")
@@ -15,27 +15,45 @@ def read_ofm_line(file, ofm_size, cho, tile):
     ]
 
     lines_with_channel = []
-    tile_chunk_size = tile * cho
+    tile_chunk_size = tile_len * cho
     for line_nc in lines_no_channel:
         line = [
             line_nc[i : i + tile_chunk_size]
             for i in range(0, len(line_nc), tile_chunk_size)
         ]
-        print(line)
-    print(ofm_size % tile)
+
+        tile_last_with_chan = None
+        line_with_channel = []   
+        
+        oft = ofm_size%tile_len
+        if oft != 0:
+            tile_last = line.pop()
+            tile_last_with_chan =  np.array([tile_last[i:i+oft] for i in range(0,len(tile_last),oft)])
+
+        for tile in line:
+            tile_with_chan = np.array([tile[i:i+tile_len] for i in range(0,len(tile),tile_len)])
+            line_with_channel.append(tile_with_chan)
+
+        if oft != 0:
+            line_with_channel.append(tile_last_with_chan)
+
+        line_with_channel = np.concatenate(line_with_channel,axis=1)
+        lines_with_channel.append(line_with_channel)
+
+    lines_with_channel = np.vstack(lines_with_channel)
+    print(lines_with_channel.shape)
 
     return lines_no_channel
 
 
 def get_act_ofm(ofm_size, cho, tile):
-    ofm_lines = []
+    ofm_lines = np.zeros((ofm_size,ofm_size))
     for i in range(8):
         file = f"../../data/act/ofm_tile_lines_{i:0d}.txt"
         ofm = read_ofm_line(file, ofm_size, cho, tile)
-        # print(ofm)
-        break
-        # ofm_lines.append(ofm)
-        # print(len(ofm))
+        ofm_lines[i::8, :] = ofm
+    print(ofm_lines)
+
 
 
 # with open("../../data/exp/ofm_dec_c32_h98_w98.txt") as f:
